e197149b782ab085dea777e6bdcac68346ce15f0
beniiiiii <56236600+beniiiiii@users.noreply.github.com>
2024-10-21 13:00:40 +0200

    Initialize project

---
+++ src/pages/addProduct/AddProduct.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./AddProduct.scss"
+
+export default function AddProduct() {
+  return (
+    <div className='addProductPage'>AddProduct</div>
+  )
+}
---
+++ src/pages/productList/ProductList.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./ProductList.scss"
+
+export default function ProductList() {
+  return (
+    <div className='productListPage'>ProductList</div>
+  )
+}
